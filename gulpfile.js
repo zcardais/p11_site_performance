292badeb4111818d24afb9152e9f0a6912bedc4c
zcardais <zcardais@gmail.com>
2017-05-08 15:06:51 -0400

    Added gulp tasks to minify and rename styles.css and scripts.js per feedback from Treehouse reviewer.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,19 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var gulpif = require('gulp-if');
 var sprity = require('sprity');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
+
+// generate sprite.png and sprite.css
+gulp.task('sprites', function () {
+  return sprity.src({
+    src: './img/avatars/**/*.{png,jpg}',
+    style: './sprite.css'
+  })
+    .pipe(gulpif('*.jpg', gulp.dest('./img/avatars/'), gulp.dest('./img/avatars/')))
+});
 
 gulp.task("concatStyles", function() {
   gulp.src([
@@ -14,7 +26,8 @@ gulp.task("concatStyles", function() {
     'css/hero.css',
     'css/photo-grid.css',
     'css/modals.css',
-    'css/footer.css'])
+    'css/footer.css',
+    '../img/avatars/sprite.css'])
     .pipe(concat("styles.css"))
     .pipe(gulp.dest("css"));
 });
@@ -30,13 +43,18 @@ gulp.task("concatScripts", function() {
   .pipe(gulp.dest("js"));
 });
 
-// generate sprite.png and _sprite.scss
-gulp.task('sprites', function () {
-  return sprity.src({
-    src: './img/avatars/**/*.{png,jpg}',
-    style: './sprite.css'
-  })
-    .pipe(gulpif('*.jpg', gulp.dest('./img/avatars/'), gulp.dest('./img/avatars/')))
+gulp.task("minifyStyles", function() {
+  gulp.src("css/styles.css")
+    .pipe(cleanCSS())
+    .pipe(rename('styles-min.css'))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task("minifyScripts", function() {
+  gulp.src("js/scripts.js")
+    .pipe(uglify())
+    .pipe(rename('scripts-min.js'))
+    .pipe(gulp.dest('js'));
 });
 
 
